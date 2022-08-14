b7199fa6a3b6707baa971c26e0271b79def5ba49
destroylord <masapin68@gmail.com>
2022-08-14 10:13:13 +0700

    feat: add feature testimonials (#7)

---
+++ app/Filament/Resources/TestimonialResource/Pages/CreateTestimonial.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\TestimonialResource\Pages;
+
+use App\Filament\Resources\TestimonialResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateTestimonial extends CreateRecord
+{
+    protected static string $resource = TestimonialResource::class;
+}
